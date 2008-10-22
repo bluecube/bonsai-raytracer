0a33abfa4914566cd0fa5e3a8b9262c98ed0a854
_cube_ <_cube_@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-10-22 16:38:54 +0000

    long options
    git-svn-id: http://svn.assembla.com/svn/bonsai_raytracer/trunk@3 e0e46c49-be69-4f5a-ad62-21024a331aea

--- client.cpp
+++ client.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 
+const unsigned long DEFAULT_PORT = 91823;
+const unsigned int DEFAULT_GRANULARITY = 8;
+
 void usage(char *name){
-	printf("Usage: %s [OPTION] [FILE]\n", name);
+	printf("Usage: %s [OPTIONS] [FILE]\n", name);
 	printf("Render a scene from FILE.\n\n");
 
-	printf("\t-s SERVER[:PORT][,SERVER[:PORT]...]\tRendering server(s) to connect to.\n");
-	printf("\t\t\tIf port is ommited use the default %i.\n", PORT);
-	printf("\t-g NUMBER\tGranularity of the rendering -- how many chunks does each\n")
-	printf("\t\t\tthread of a rendering server have to render on average. Default 8.");
-	printf("\t-r [PORT]\t\tLaunch a local rendering server.\n");
-	printf("\t-h\t\tThis help.\n");
+	printf("\t-s, --server=SERVER[:PORT][,SERVER[:PORT]...]\n");
+	printf("\t\tRendering server(s) to connect to.\n");
+	printf("\t\tIf port is ommited use the default %i.\n", DEFAULT_PORT);
+	printf("\t-g, --granularity=NUMBER\n");
+	printf("\t\tGranularity of the rendering -- how many chunks does each\n")
+	printf("\t\tthread of a rendering server have to render on average. Default %i.", DEFAULT_GRANULARITY);
+	printf("\t-r, --local-renderer[=PORT]\n");
+	printf("\t\tLaunch a local rendering server.\n");
+	printf("\t\tIf port is ommited use the default %i.\n", DEFAULT_PORT);
+	printf("\t-h, --help\n");
+	printf("\t\tThis help.\n");
 }
 
 int main(int argc, char **argv){
@@ -18,7 +26,20 @@ int main(int argc, char **argv){
 
 	printf("Bonsai rayracer client\n");
 
-	while((opt = getopt(argc, argv, "s:r::g:h")) != -1){
+	while(1){
+		int optionIndex = 0
+		static struct option[] = {
+			{"server", 1, 0, 's'},
+			{"granularity", 1, 0, 'g'},
+			{"local-renderer", 2, 0, 'r'},
+			{"help", 0, 0, 'h'},
+			{0, 0, 0, 0},
+		} longopts;
+		opt = getopt_long(argc, argv, "s:r::g:h", longopts, &optionIndex);
+
+		if(opt == -1)
+			break;
+
 		switch(opt){
 		case 's':
 			
