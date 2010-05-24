2a3c0a1395811c2ae24847d813040f9010bb7afd
Kuba Marek <blue.cube@seznam.cz>
2010-05-25 01:51:04 +0200

    Added file name, line number and function name to error messages.

--- client/util.c
+++ client/util.c
@@ -9,8 +9,10 @@
 /**
  * Print a formated error message.
  */
-static void print_error(int error, const char *fmt, va_list va){
-	flockfile(stderr);
+static void print_error(int error, const char *file, int line,
+	const char *func, const char *fmt, va_list va){
+	
+	fprintf(stderr, "%s:%i/%s(): ", file, line, func);
 
 	if(fmt){
 		vfprintf(stderr, fmt, va);
@@ -22,24 +24,18 @@ static void print_error(int error, const char *fmt, va_list va){
 		}
 		fputs(strerror(error), stderr);
 	}
-
-	fputc('\n', stderr);
-	
-	funlockfile(stderr);
 }
 
 /**
  * Print a message and explanation of error code
  * to stderr and exit with EXIT_FAILURE.
- * \param error Errno or 0 if error description
- * should not be printed.
- * \param fmt Custom error message or NULL.
  */
-void error(int error, const char *fmt, ...){
+void error_internal(int e, const char *file, int line,
+	const char *func, const char *fmt, ...){
 	va_list va;
 
 	va_start(va, fmt);
-	print_error(error, fmt, va);
+	print_error(e, file, line, func, fmt, va);
 	va_end(va);
 
 	exit(EXIT_FAILURE);
@@ -47,15 +43,13 @@ void error(int error, const char *fmt, ...){
 
 /**
  * Print a message and explanation of error code to stderr.
- * \param error Errno or 0 if error description
- * should not be printed.
- * \param fmt Custom error message or NULL.
  */
-void warning(int error, const char *fmt, ...){
+void warning_internal(int e, const char *file, int line,
+	const char *func, const char *fmt, ...){
 	va_list va;
 
 	va_start(va, fmt);
-	print_error(error, fmt, va);
+	print_error(e, file, line, func, fmt, va);
 	va_end(va);
 }
 
--- client/util.h
+++ client/util.h
@@ -5,8 +5,30 @@
 
 void debug(const char *fmt, ...);
 
-void error(int error, const char *fmt, ...);
-void warning(int error, const char *fmt, ...);
+/**
+ * Print a message and explanation of error code
+ * to stderr and exit with EXIT_FAILURE.
+ * \param e Errno or 0 if error description
+ * should not be printed.
+ * \param fmt Custom error message or NULL.
+ */
+#define error(e, format, ...) \
+	error_internal((e), __FILE__, __LINE__, __func__, (format), ##__VA_ARGS__)
+
+/**
+ * Print a message and explanation of error code to stderr.
+ * \param e Errno or 0 if error description
+ * should not be printed.
+ * \param fmt Custom error message or NULL.
+ */
+#define warning(e, format, ...) \
+	warning_internal((e), __FILE__, __LINE__, __func__, (format), ##__VA_ARGS__)
+		
+
+void error_internal(int e, const char *file, int line,
+	const char *func, const char *fmt, ...);
+void warning_internal(int e, const char *file, int line,
+	const char *func, const char *fmt, ...);
 
 void *checked_malloc(size_t size);
 void *checked_realloc(void *ptr, size_t size);
