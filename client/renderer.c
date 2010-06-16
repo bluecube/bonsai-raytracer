44a20804f4066d5e2f6240808447f48ed22be002
Kuba Marek <blue.cube@seznam.cz>
2010-06-16 03:11:38 +0200

    Very simple ray rendering.

--- client/renderer.c
+++ client/renderer.c
@@ -2,12 +2,29 @@
 
 #include <math.h>
 
-#include "color.h"
+#include "measurements.h"
+#include "object.h"
 #include "random.h"
 #include "ray.h"
+#include "photon.h"
 
-void renderer_render_ray(struct scene *s, struct ray *r, struct color *result){
-	
+/**
+ * Render a single ray from a camera.
+ * \param s Scene to render.
+ * \param r Ray being cast.
+ * \param[in,out] Wavelength of the ray. The w->wavelength is an input, and w->value is returned.
+ * \todo Most of the rendering work should be here. Now there is only a trivial shading used here.
+ */
+static void render_ray(struct scene *s, struct ray *r, struct photon *p){
+	struct object *obj;
+	float distance = scene_ray_intersection(s, r, &obj);
+
+	if(distance < 0){
+		// If the ray didn't hit anything, the ray stays black.
+		return;
+	}
+
+	p->energy = 0.5;
 }
 
 /**
@@ -17,12 +34,15 @@ void renderer_render_ray(struct scene *s, struct ray *r, struct color *result){
  * \param y1 First row after the block to be rendered.
  * \param pixmap Pointer to pixel map with result.
  * Must be large enough to hold #y0 - #y1 rows.
+ * \todo Lens simulation!
  */
 void renderer_render(struct scene *s, unsigned y0, unsigned y1, struct color *pixmap){
+	MEASUREMENTS_WARMUP();
+	MEASUREMENTS_START();
+
 	float inc = 1 / (s->width - 1);
 
 	float yy = inc * (y0 - s->height / 2);
-	float weight = 1 / s->raysPerPx;
 	for(unsigned y = y0; y < y1; ++y){
 		float xx = -0.5;
 		for(unsigned x = 0; x < s->width; ++x){
@@ -37,16 +57,19 @@ void renderer_render(struct scene *s, unsigned y0, unsigned y1, struct color *pi
 					s->focalLength,
 					&r.direction);
 				
-				struct color result;
-				renderer_render_ray(s, &r, &result);
+				struct photon p;
+				photon_random_init(&p);
 
-				color_add(pixmap, &result);
+				render_ray(s, &r, &p);
+				
+				photon_add_to_color(&p, pixmap);
 			}
-			color_scale(pixmap, weight);
 			++pixmap;
 
 			xx += inc;
 		}
 		yy += inc;
 	}
+
+	MEASUREMENTS_PRINT();
 }
--- client/renderer.h
+++ client/renderer.h
@@ -1,6 +1,9 @@
 #ifndef BONSAI_CLIENT_RENDERER_H
 #define BONSAI_CLIENT_RENDERER_H
 
+#include "color.h"
 #include "scene.h"
+
+void renderer_render(struct scene *s, unsigned y0, unsigned y1, struct color *pixmap);
 
 #endif
